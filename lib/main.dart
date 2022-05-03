de418b15ab93957b415dc3ee849829d0f4f888f2
qifen <858810078@qq.com>
2022-05-03 11:21:08 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -7,109 +7,100 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  DragDownDetails? _down;
+  DragEndDetails? _end;
+  DragUpdateDetails? _update;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double size =
+        MediaQuery.of(context).size.width < MediaQuery.of(context).size.height
+            ? MediaQuery.of(context).size.width
+            : MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          child: GestureDetector(
+        child: CustomPaint(
+          painter: Cp(_down, _end, _update),
+          size: Size(size, size),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPanDown: (c) {
+          print('down');
+          setState(() {
+            _down = c;
+          });
+        },
+        onPanEnd: (c) {
+          print('end');
+
+          setState(() {
+            _end = c;
+          });
+        },
+        onPanUpdate: (c) {
+          print('update');
+
+          setState(() {
+            _update = c;
+          });
+        },
+      )),
     );
+  }
+}
+
+class Cp extends CustomPainter {
+  Paint _paint = new Paint()
+    ..color = Colors.red //画笔颜色
+    ..strokeCap = StrokeCap.round //画笔笔触类型
+    ..isAntiAlias = true //是否启动抗锯齿
+    ..style = PaintingStyle.stroke //绘画风格，默认为填充
+    ..filterQuality = FilterQuality.high //颜色渲染模式的质量
+    ..strokeWidth = 2; //画笔的宽度
+
+  DragDownDetails? _down;
+  DragEndDetails? _end;
+  DragUpdateDetails? _update;
+
+  Cp(this._down, this._end, this._update);
+
+  // ..blendMode = BlendMode.exclusion //颜色混合模式
+  // ..colorFilter = ColorFilter.mode(Colors.red,
+  // BlendMode.exclusion) //颜色渲染模式，一般是矩阵效果来改变的，但   是flutter中只能使用颜色混合模式
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2),
+        (size.width < size.height ? size.width : size.height) / 2, _paint);
+
+
+
+
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
